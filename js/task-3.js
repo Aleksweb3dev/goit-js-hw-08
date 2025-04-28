74797f87c09ab428d7e0285e2c90f647050e5fcc
Aleksandr Roda <execzweb3dev@gmail.com>
2025-04-28 19:39:59 +0200

    initial

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const inputName = document.querySelector('#name-input');
-inputName.addEventListener('input', addOutput);
-function addOutput() {
-  const output = document.querySelector('#name-output');
-  output.textContent = inputName.value.trim() || 'Anonymous';
-}
